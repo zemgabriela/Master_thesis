e44da94fddcf9dbd8a53e28540e4fea469bc158f
Gabriela Zemencikova <gabrielazemencikova@Gabrielas-MacBook-Pro-2.local>
2024-06-12 20:17:16 +0200

    Data cleaning updated

--- Data Cleaning/modelling_dataset(data).py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Read the CSV files
-cardio = pd.read_csv('modelling_data/cardio_at_asir.csv')
-environment = pd.read_csv('modelling_data/environment.csv')
-
-# Merge the DataFrames with an indicator
-combined = environment.merge(cardio, on=['date', 'at_code'], how='right', indicator=False)
-combined.to_csv('modelling_data/data.csv', index = False)
